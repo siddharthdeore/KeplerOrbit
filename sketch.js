d4fcddddd2eb0d8abb99ccb888a907f902ae07c5
SIDDHARTH DEORE <siddharthdeore@gmail.com>
2019-03-22 13:28:56 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,6 +5,9 @@ var omega=0.3;
 var RA=0.6;
 var inc=-0.6;
 var plist=[];
+var peri_x,peri_y,peri_z;
+var apo_x,apo_y,apo_z;
+
 function setup() {
 	createCanvas(windowWidth, windowHeight,WEBGL);
 	//createCanvas(windowWidth, windowHeight);
@@ -13,33 +16,88 @@ function setup() {
 	background(0);
 	//line(windowWidth/2,0,windowWidth/2,windowHeight);
 	smooth();
+	update();
 }
 function draw() {
+
+	render();
+
+	//for (var i = plist.length - 1; i >= 0; i--) {
+		//plist[frameCount%200].show();
+	//}
+
+}
+function render() {
 	background(0);
 	rotateX(1.4);
 	rotateZ(-1);
 	ambientLight(160, 160, 160);
 	pointLight(255, 255, 255, 300, 300, 100);
 
-	//getConics(a,e);
-	getKeplerOrbit(a,e,0,RA,omega)
-	//for (var i = plist.length - 1; i >= 0; i--) {
-		//plist[frameCount%200].show();
-	//}
-
-	orbitControl();
-}
-
-
-function getKeplerOrbit(a,e,i,RA,omega) {
-	//background(255);
-	let p = 0;
-	let min = -1;
-	let max = 1;
 	stroke(100,100,100,100);
 	line(-500,0,0,500,0,0);
 	line(0,-500,0,0,500,0);
 	line(0,0,-500,0,0,500);
+	stroke(255,40,70,255);
+	
+	strokeWeight(2);
+	prev_x=peri_x;
+	prev_y=peri_y;
+	prev_z=peri_z;
+
+	for (var i = plist.length - 1; i >= 0; i--) {
+		x=plist[i].x;
+		y=plist[i].y;
+		z=plist[i].z;
+
+		if(dist(x,y,z,prev_x,prev_y,prev_z)<a/2)
+			line(x,y,z,prev_x,prev_y,prev_z);
+		strokeWeight(1);
+
+		prev_x=x;
+		prev_y=y;
+		prev_z=z;
+	}
+
+	noStroke();
+	push();
+	translate(0,0,0);
+	specularMaterial(0,255,200,230);
+	sphere(20);
+	ambientMaterial(200,155,155,32);
+	plane(800,800);
+	pop();
+	push();
+	translate(sat_x,sat_y,sat_z);
+	specularMaterial(255,255,255,220);
+	box(10);
+	pop();
+
+
+	push();
+	b=a*Math.sqrt(Math.abs(e*e-1));
+	rotateZ(RA);
+	rotateX(inc);
+	rotateZ(-omega);
+
+	specularMaterial(255,255,255,32);
+	plane(600,600);
+	pop();
+	stroke(120,150);
+	line(0,0,0,sat_x,sat_y,sat_z);
+	line(apo_x,apo_y,apo_z,peri_x,peri_y,peri_z);
+	//update();
+	orbitControl();
+
+}
+
+function update() {
+	getKeplerOrbit(a,e,inc,RA,omega)
+}
+function getKeplerOrbit(a,e,i,RA,omega) {
+	let p = 0;
+	let min = -1;
+	let max = 1;
 	//line(0,windowHeight/2,windowWidth,windowHeight/2);
 	let f = a;
 	noStroke();
@@ -81,9 +139,6 @@ function getKeplerOrbit(a,e,i,RA,omega) {
 	N=200;
 	[peri_x,peri_y,peri_z] =getECI(e,p,0,omega,RA,inc);
 
-	prev_x=peri_x;
-	prev_y=peri_y;
-	prev_z=peri_z;
 	plist=[];
 	E=0;t=0;
 	for (var i = 1; i < N; i++) {
@@ -91,15 +146,7 @@ function getKeplerOrbit(a,e,i,RA,omega) {
 		// 3d plot
 		[x,y,z]=getECI(e,p,t,omega,RA,inc);
 		// orbit
-		stroke(255,40,70,255);
-		strokeWeight(2);
-		if(dist(x,y,z,prev_x,prev_y,prev_z)<a/2)
-			line(x,y,z,prev_x,prev_y,prev_z);
-			strokeWeight(1);
 
-		prev_x=x;
-		prev_y=y;
-		prev_z=z;
 		plist.push(new Particle(x,y,z));
 
 	}
@@ -108,33 +155,7 @@ function getKeplerOrbit(a,e,i,RA,omega) {
 	[E,t]=invKepler(M0,e);
 	[sat_x,sat_y,sat_z] = getECI(e,p,t,omega,RA,inc);
 	//line(peri_x,peri_y,peri_z,apo_x,apo_y,apo_z)
-	noStroke();
-	push();
-	translate(0,0,0);
-	specularMaterial(0,255,200,230);
-	sphere(20);
-	ambientMaterial(200,155,155,32);
-	plane(800,800);
-	pop();
-	push();
-	translate(sat_x,sat_y,sat_z);
-	specularMaterial(255,255,255,220);
-	box(10);
-	pop();
 
-
-	push();
-	b=a*Math.sqrt(Math.abs(e*e-1));
-	rotateZ(RA);
-	rotateX(inc);
-	rotateZ(-omega);
-
-	specularMaterial(255,255,255,32);
-	plane(600,600)
-	pop();
-	stroke(120,150);
-	line(0,0,0,sat_x,sat_y,sat_z)
-	line(apo_x,apo_y,apo_z,peri_x,peri_y,peri_z)
 
 
 }
@@ -202,22 +223,28 @@ function invKepler(M,e) {
 /////////////////UI functions/////////////////////////
 function setecc(arg) {
 	e=arg;
+	update();
 }
 function seta(arg) {
 	a=arg;
+	update();
 }
 function setinc(arg) {
 	inc=arg*Math.PI/180;
+	update();
 }
 function setM0(arg) {
 	M0=arg*Math.PI/180;
+	update();
 }
 function setomega(arg) {
 	omega=arg*Math.PI/180;
+	update();
 }
 
 function setRA(arg) {
 	RA=arg*Math.PI/180;
+	update();
 }
 
 function Particle(x,y,z) {
@@ -235,76 +262,3 @@ Particle.prototype.show = function() {
 	pop();
 
 };
-
-/////
-/*
-function getConics(a,e) {
-	background(255);
-	let p = 0;
-	let min = -1;
-	let max = 1;
-	stroke(100,100,100);
-	line(windowWidth/2,0,windowWidth/2,windowHeight);
-	line(0,windowHeight/2,windowWidth,windowHeight/2);
-	let f = a;
-	noStroke();
-	if (e==0) {
-		text("circle",10,10);
-		p=a*(1-e*e);
-		min = -Math.PI;
-		max = Math.PI;
-		f = a*e;
-	}
-	else if (e>0 && e<1) {
-		text("ellipse",10,10);
-		p=a*(1-e*e);
-		min = -Math.PI;
-		max = Math.PI;
-		f = a*e;
-	}
-	else if (e==1) {
-		text("Parabola",10,10);
-		p=a;
-		min = -Math.PI;
-		max = Math.PI;
-		f = (a*e-a);
-	}
-	else {
-		text("Hyperbola",10,10);
-		p=a*(e*e-1);
-		min = -Math.PI;
-		max = Math.PI;
-		f = 2*a-a*e;
-	}
-	text("a="+a,10,20);
-	text("e="+e,10,30);
-
-	stroke(0);
-	N=1000;
-	step=(max-min)/N;
-	prev_x=a;
-	prev_y=0;
-	plist=[];
-	for (var i = 0; i < N; i++) {
-		//t=i*step;
-		t=map(i,0,N-1,min,max)
-		r=p/(1+e*cos(t));
-		push();
-		translate(windowWidth/2,windowHeight/2);
-		x=r*cos(t)+0*f;
-		y=r*sin(t);
-		//x2=a*cos(t);
-		//y2=a*sin(t);
-		{
-			ellipse(x,y,1,1);
-		//ellipse(x2,y2,1,1);
-		//ellipse(0,0,5,5);
-		//ellipse(-f,0,5,5);
-	}
-	prev_x=x;
-	prev_y=y;
-	pop();
-	plist.push(new Particle(x,y));
-}
-}
-*/
